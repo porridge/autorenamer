f50a97b80ad556cdbdef8e073ccb5c0c98845941
Marcin Owsiany <marcin@owsiany.pl>
2021-02-01 08:36:02 +0100

    Bump version and fix typo.

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='Autorenamer',
-      version='0.5',
-      description='Rrename files to make them sort in given order.',
+      version='0.6',
+      description='Rename files to make them sort in given order.',
       author='Marcin Owsiany',
       author_email='marcin@owsiany.pl',
       url='http://marcin.owsiany.pl/autorenamer-page',
